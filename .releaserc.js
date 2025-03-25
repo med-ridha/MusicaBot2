b1ceee1939958608840690606cc7aff85af2a9e2
med-ridha <ridha.zemzem@med.tn>
2025-03-25 13:29:24 +0100

    fix: backmergeStrategy to merge

--- .releaserc.js
+++ .releaserc.js
@@ -1,42 +1,44 @@
 const country = process.env.COUNTRY;
 module.exports = {
-  branches: ["master" ],
-  tagFormat: country + "-v${version}",
-  plugins: [
-    [
-      "@semantic-release/commit-analyzer",
-      {
-        preset: "conventionalcommits"
-      }
+    branches: ["master"],
+    tagFormat: country + "-v${version}",
+    plugins: [
+        [
+            "@semantic-release/commit-analyzer",
+            {
+                preset: "conventionalcommits"
+            }
+        ],
+        "@semantic-release/release-notes-generator",
+        [
+            "@semantic-release/github",
+            {
+                successComment: false,
+                failComment: false,
+                assets: [],
+            }
+        ],
+        [
+            "@semantic-release/changelog",
+            {
+                changelogFile: "CHANGELOG.md"
+            }
+        ],
+        [
+            "@semantic-release/git",
+            {
+                message: `Release(${country}):` + " ${nextRelease.version}" + ` - ${new Date().toLocaleDateString("en-GB")}\n\n` + "${nextRelease.notes}"
+            }
+        ],
+        [
+            "@saithodev/semantic-release-backmerge",
+            {
+                backmergeStrategy: merge,
+            }
+        ],
     ],
-    "@semantic-release/release-notes-generator",
-    [
-      "@semantic-release/github",
-      {
-        successComment: false,
-        failComment: false,
-        assets: [],
-      }
-    ],
-    [
-      "@semantic-release/changelog",
-      {
-        changelogFile: "CHANGELOG.md"
-      }
-    ],
-    [
-      "@semantic-release/git",
-      {
-        message: `Release(${country}):` + " ${nextRelease.version}" + ` - ${new Date().toLocaleDateString("en-GB")}\n\n` + "${nextRelease.notes}"
-      }
-    ],
-    [
-      "@saithodev/semantic-release-backmerge",
-      {
-        branches: ["master"]
-      }
+    "branches": [
+        { name: "master", prerelease: "develop" }
     ]
-  ]
-
 };
 
